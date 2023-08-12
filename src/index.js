ef71e0c1294189db78ae03e9f4542e504d5d8276
tex008 <texera009@gmail.com>
2023-08-11 23:33:44 -0300

    chore: insert variable which defines the new dialog id to addeventlistener callback and inserts a coment in the append child

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ window.onload = () => {
   const notificationTemplate = document.querySelector("#notification");
   const notificationContainer = document.getElementById("toast-notification-container");
   const showNotificationButton = document.getElementById("show-notification-button");
-  
-  // variable which defines the if for the new dialog notification
-  const newDialogId = `notification-${Date.now()}`;
+
   
 
   showNotificationButton.addEventListener("click", () => {
     // insert a new toast notification
     const newNotification = notificationTemplate.content.cloneNode(true);
+    
+    // variable which defines the if for the new dialog notification
+    const newDialogId = `notification-${Date.now()}`;
 
     // set the id for the new toast notification
     const notificationDialog = newNotification.querySelector("dialog")
@@ -20,6 +21,7 @@ window.onload = () => {
       event.target.remove()
     })
 
+    // insert the new toast notification in the DOM
     notificationContainer.appendChild(newNotification)
 
     // when the toast are created, after 4 seconds, it's automatically removed from DOM
