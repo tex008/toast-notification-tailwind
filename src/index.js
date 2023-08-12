2f7a81383438249e081c2239bababec2f9dbda7b
tex008 <texera009@gmail.com>
2023-08-11 23:31:45 -0300

    fix: fix the render logic and adds dialog close logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 window.onload = () => {
-  const notificationTemplate = document.getElementById("toast-notification-component");
+  // elements declaration
+  const notificationTemplate = document.querySelector("#notification");
   const notificationContainer = document.getElementById("toast-notification-container");
   const showNotificationButton = document.getElementById("show-notification-button");
+  
+  // variable which defines the if for the new dialog notification
+  const newDialogId = `notification-${Date.now()}`;
+  
 
   showNotificationButton.addEventListener("click", () => {
-    
-    console.log(notificationTemplate)
+    // insert a new toast notification
     const newNotification = notificationTemplate.content.cloneNode(true);
+
+    // set the id for the new toast notification
+    const notificationDialog = newNotification.querySelector("dialog")
+    notificationDialog.id = newDialogId
+    // adds a remove event on the notification when the button Close is clicked
+    notificationDialog.addEventListener("close", (event) => {
+      event.target.remove()
+    })
+
     notificationContainer.appendChild(newNotification)
+
+    // when the toast are created, after 4 seconds, it's automatically removed from DOM
+    setTimeout(() => {
+      const dialogToClose = document.getElementById(newDialogId)
+      if (dialogToClose) {
+        dialogToClose.remove()
+      }
+    },1000)
   })
 }
